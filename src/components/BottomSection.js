4864d0e8536748b98f9669dda6acc32f5f21cf93
Byron Povea <bpovea@duodata.io>
2021-04-21 23:07:56 -0500

    [UPD] bottom section

--- src/components/BottomSection.js
+++ src/components/BottomSection.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Github,Linkedin } from 'react-bootstrap-icons';
 
 // state less Component
 const BottomSection = () => (
     <div>
-        <h3>Bottom Section!</h3>
+        <h5>Powered by <a href="https://bpovea.github.io/" target="_blank" rel="noreferrer">Byron Povea</a></h5>
+        <h5><a href="https://github.com/bpovea" target="_blank" rel="noreferrer"><Github/> GitHub</a></h5>
+        <h5><a href="https://www.linkedin.com/in/bpovea/" target="_blank" rel="noreferrer"><Linkedin/> Linkedin</a></h5>
     </div>
 );
 
