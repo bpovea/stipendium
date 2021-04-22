b96d6f742cea20c25baf403d74059ae1b5fe5b80
Byron Povea <bpovea@duodata.io>
2021-04-22 11:13:24 -0500

    [UPD] direct link to github project

--- src/components/BottomSection.js
+++ src/components/BottomSection.js
@@ -6,8 +6,9 @@ import { Github,Linkedin } from 'react-bootstrap-icons';
 const BottomSection = () => (
     <div>
         <h5>Powered by <a href="https://bpovea.github.io/" target="_blank" rel="noreferrer">Byron Povea</a></h5>
-        <h5><a href="https://github.com/bpovea" target="_blank" rel="noreferrer"><Github/> GitHub</a></h5>
-        <h5><a href="https://www.linkedin.com/in/bpovea/" target="_blank" rel="noreferrer"><Linkedin/> Linkedin</a></h5>
+        <h5>See Code <a href="https://github.com/bpovea/stipendium" target="_blank" rel="noreferrer">HERE</a></h5>
+        <h5>More projects like this on <a href="https://github.com/bpovea" target="_blank" rel="noreferrer"><Github/> My GitHub Profile</a></h5>
+        <h5>Followme on <a href="https://www.linkedin.com/in/bpovea/" target="_blank" rel="noreferrer"><Linkedin/> Linkedin</a></h5>
     </div>
 );
 
